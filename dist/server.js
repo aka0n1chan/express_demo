c18a89b3d4acd514d9817878df1381c34cfce1dc
aka0n1chan <oniiwaakari@oniiwaakarinoMacBook-Air.local>
2021-06-12 22:23:50 +0900

    First commit

--- dist/server.js
+++
@@ -1,2 +0,0 @@
-(()=>{"use strict";var e=require("express")();e.get("/",(function(e,n){n.send("hello world")})),e.listen(3e3,(function(){console.log("Listen on port 3000")}))})();
-//# sourceMappingURL=server.js.map
